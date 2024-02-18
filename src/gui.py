cfb3f33b24bae96e86ef463bd26030bc8a70fa2f
Guilherme M. de Souza <guilherme.meneghinsouza@gmail.com>
2024-02-18 20:16:32 -0300

    Implementing num of pids

--- src/gui.py
+++ src/gui.py
@@ -8,6 +8,28 @@ from pid import Pid
 # title
 TITLE_ASSET = './assets/title.txt'
 
+# simulaiton_setup -> conteiner
+SIMULATION_SETUP_ASSET = './assets/setup/conteiner.txt'
+# simulaiton_setup -> inputs
+NUM_PIDS_ASSET = './assets/setup/num_pids.txt'
+
+# Simulation Setting
+MAX_NPIDS = 9
+
+
+class SimulationSetup(Conteiner):
+    def __init__(self,
+                 conteiner: _CursesWindow,
+                 assetfile: str,
+                 attr=curses.A_NORMAL):
+        super().__init__(conteiner, assetfile, attr)
+        self.create_win()
+
+    def generate_components(self, numpids_trigger, start_triiger):
+        self.components_list = [
+            TextInput(self.win, NUM_PIDS_ASSET, trigger=numpids_trigger)
+        ]
+
 
 class PidManager():
     def __init__(self, npids):
@@ -32,9 +54,19 @@ class PidManager():
 class Gui():
     def __init__(self, stdscr):
         self.title = Text(stdscr, TITLE_ASSET, curses.A_REVERSE)
+        self.simu_setup_conteiner = SimulationSetup(stdscr, SIMULATION_SETUP_ASSET)
+        self.pid_manager = PidManager(MAX_NPIDS)
+
+        self.simu_setup_conteiner.generate_components(
+            self.set_num_of_pids, # num pids trigger
+        )
 
     def draw(self, autorefresh=False):
         self.title.draw(autorefresh)
+    
+    def set_num_of_pids(self, npids_str: str):
+        """Num of PIDs trigger."""
+        npids = int(npids_str)
 
 
 def main(stdscr: _CursesWindow):
