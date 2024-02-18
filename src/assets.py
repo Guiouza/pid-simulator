336ab8817ab98b31128d75384c93520c7a72a22c
Guilherme M. de Souza <guilherme.meneghinsouza@gmail.com>
2024-02-18 20:01:30 -0300

    Implementing a Text Input Area class for handling input assets.

--- src/assets.py
+++ src/assets.py
@@ -26,6 +26,11 @@ def read_asset(fname) -> dict[str: str|int]:
         case 'button':
             asset.setdefault('cursor_y', int(type_info[1]))
             asset.setdefault('cursor_x', int(type_info[2]))
+        case 'input':
+            asset.setdefault('input_y', int(type_info[1]))
+            asset.setdefault('input_x', int(type_info[2]))
+            asset.setdefault('max_len', int(type_info[3]))
+            asset.setdefault('default', str.join('', type_info[4:]))
 
     return asset
 
@@ -166,3 +171,122 @@ class Button(Text):
     def reset_attr(self, autorefresh=True):
         self.labelattr = self.original_attr
         return super().reset_attr(autorefresh)
+
+
+class TextInput(Text):
+    def __init__(self,
+                 conteiner: _CursesWindow,
+                 assetfile: str,
+                 attr=curses.A_NORMAL,
+                 labelattr=curses.A_REVERSE,
+                 filterfunc=str.isprintable,
+                 trigger=None ):
+        super().__init__(conteiner, assetfile, attr)
+        self.filter = filterfunc
+        self.labelattr = labelattr
+        self.original_attr = self.attr
+        self.original_labelattr = self.labelattr
+        self.current_content = self.asset['default']
+        self.trigger = trigger
+
+    def __call__(self):
+        """
+        Initiate insertion mode, the filter only allow the right caracters to 
+        be inserted, the user can let the input empty to insert the default value
+        """
+        old_labelattr = self.labelattr
+
+        # set terminal attributes
+        self.chlabelattr(self.attr)
+        curses.curs_set(2)
+        self.win.nodelay(True)
+
+        self.win.move(*self.get_cursor_relyx())
+        while True:
+            # get input ch
+            try:
+                key = self.win.get_wch()
+            except:
+                key = None
+
+            match (key):
+                # pass
+                case None: continue
+                # check for a enter
+                case '\n': break
+                # check for a backspace
+                case 'KEY_BACKSPACE' | '\b' | '\x7f' | '^?':
+                    if self.current_content != '':
+                        # delete last character
+                        self.current_content = self.current_content[:-1]
+
+                        self.win.addch(*self.get_cursor_relyx(), ' ')
+                        self.win.move(*self.get_cursor_relyx())
+                case _:
+                    if len(self.current_content) >= self.asset['max_len']:
+                        continue
+
+                    if self.filter(self.current_content + str(key)):
+                        self.win.addch(key, self.labelattr)
+                        self.current_content += key
+
+        # verify if was given a input
+        if self.current_content == '' and self.asset['default'] != '':
+            self.current_content = self.asset['default']
+        
+        # return terminal to prev state
+        self.chlabelattr(old_labelattr, True)
+        self.win.nodelay(False)
+        curses.curs_set(0)
+
+        self.trigger(self.current_content)
+
+    def chattr(self, attr, autorefresh=True):
+        self.attr = attr
+        super().draw(autorefresh)
+
+    def chlabelattr(self, attr, autorefresh=True):
+        """
+        Change the of the label attribute and redraw the window, but do not refresh.
+
+        autorefresh: bool, refresh the window so you dont need to 
+        refresh yourself using the conteiner window, default is False.
+        For optimization is recomended to make all changes in the windows then refresh.
+        """
+        self.labelattr = attr
+        self.draw(autorefresh)
+
+    def draw(self, autorefresh=False):
+        # draw the base asset
+        super().draw(autorefresh)
+
+        try:
+            # draw label content
+            self.win.addstr(
+                self.asset['input_y'],
+                self.asset['input_x'],
+                self.current_content,
+                self.labelattr
+            )
+        except:   
+            # cursor exceeds the windows size
+            self.win.move(self.asset['input_y'], self.asset['input_y'])
+
+        # auto refresh
+        if autorefresh:
+            self.win.refresh()
+
+    def get_cursor_relyx(self) -> tuple[int, int]:
+        y = self.asset['input_y']
+        x = len(self.current_content) + self.asset['input_x']
+        return y, x
+
+    def get_cursor_absyx(self) -> tuple[int, int]:
+        y, x = self.get_cursor_relyx()
+        y += self.asset['win_y']
+        x += self.asset['win_x']
+        return y, x
+
+    def reset_attr(self, autorefresh=True):
+        self.chattr(self.original_attr, False)
+        self.chlabelattr(self.original_labelattr, autorefresh)
